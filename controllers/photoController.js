20c2688710431bebf091df0646b30642f2d5665c
Melih Kose <ramazanarc5@gmail.com>
2022-12-02 03:27:57 +0300

    mailislemleri

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -55,9 +55,16 @@ const getAllPhotos = async (req,res) => {
 const getAPhoto = async (req,res) => {
     try {
         const photo = await (await Photo.findById({_id : req.params.id})).populate("user");
+
+        let isOwner = false;
+        if(res.locals.user){
+            isOwner = photo.user.equals(res.locals.user._id);
+        }
+
         res.status(200).render("photo",{
             photo,
             link:"photos",
+            isOwner,
         });
         
     } catch (error) {
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -10,6 +10,9 @@ router.route("/about").get(pageController.getaboutPage);
 router.route("/register").get(pageController.getregisterPage);
 router.route("/login").get(pageController.getLoginPage);
 router.route("/logout").get(pageController.getLogout);
+router.route("/contact").get(pageController.getContactPage);
+router.route("/contact").post(pageController.sendMail);
+
 
 
 export default router;
